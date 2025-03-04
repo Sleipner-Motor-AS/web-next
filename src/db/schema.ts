ffddd7cee0fae4abd8a0ee8b6a0bc5fc2108f9e1
strandseter <43789401+strandseter@users.noreply.github.com>
2025-03-04 15:29:33 +0100

    feat: init customers, products and customer prices tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,24 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { integer, numeric, pgTable, varchar } from 'drizzle-orm/pg-core';
+
+export const customersTable = pgTable('customers', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  odin_id: integer().notNull().unique(),
+  customer_no: varchar({ length: 255 }).notNull().unique(),
+});
 
 export const productsTable = pgTable('products', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  odin_id: integer().notNull().unique(),
+  sku: varchar({ length: 255 }).notNull().unique(),
+  sku2: varchar({ length: 255 }).notNull().unique(),
   slug: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const customerPrices = pgTable('customerPrices', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  customer: integer().references(() => customersTable.id),
+  product: integer().references(() => productsTable.id),
+  net: numeric({ precision: 10, scale: 2 }).notNull(),
+  gross: numeric({ precision: 10, scale: 2 }).notNull(),
+  discount: numeric({ precision: 10, scale: 2 }).notNull(),
 });
