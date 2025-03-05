43d6b6b9979ddf3d6264efe612438e420db8c1b3
strandseter <43789401+strandseter@users.noreply.github.com>
2025-03-05 15:17:24 +0100

    feat: products page

--- src/markets.ts
+++ src/markets.ts
@@ -1,37 +1,46 @@
 export const markets = [
   {
     label: 'International',
-    language: 'en-US',
+    code: 'en',
+    language: 'en',
   },
   {
     label: 'Norway',
+    code: 'no',
     language: 'no-NB',
   },
   {
     label: 'Sweden',
+    code: 'sv',
     language: 'sv-SE',
   },
   {
     label: 'Denmark',
+    code: 'da',
     language: 'da-DK',
   },
   {
     label: 'UK',
-    language: 'en-GB',
+    code: 'uk',
+    language: 'en',
   },
   {
     label: 'Germany',
+    code: 'de',
     language: 'de-DE',
   },
   {
     label: 'Finland',
+    code: 'fi',
     language: 'fi-FI',
   },
   {
     label: 'Italy',
+    code: 'it',
     language: 'it-IT',
   },
 ] as const;
 
 export type Market = (typeof markets)[number];
+export type MarketCode = Market['code'];
 export type MarketLanguageCode = Market['language'];
