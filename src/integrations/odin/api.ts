aae60bda9a272b3817e7494480c5f9e17c5e4a1a
strandseter <43789401+strandseter@users.noreply.github.com>
2025-03-07 13:41:52 +0100

    feat: improve odin get api

--- src/integrations/odin/api.ts
+++ src/integrations/odin/api.ts
@@ -1,10 +1,15 @@
-// TODO: Make more robust. Simple for testing purposes.
+import { ZodError, ZodSchema } from 'zod';
+
+import { env } from '@/env';
+
+type RequestOptions = Omit<NonNullable<Parameters<typeof fetch>[1]>, 'method'>;
+
 export const api = {
-  get: async <TResponse>(endpoint: string) => {
-    const response = await fetch(endpoint);
+  get: async <TResponse>(endpoint: string, responseSchema: ZodSchema<TResponse>, options?: RequestOptions) => {
+    const response = await fetch(getUrl(endpoint), getOptions(options));
 
     if (!response.ok) {
-      throw createError(response);
+      throw createResponseError(response);
     }
 
     let json: TResponse;
@@ -12,14 +17,51 @@ export const api = {
     try {
       json = await response.json();
     } catch (_) {
-      throw createError(response);
+      throw createJsonError(response);
     }
 
-    return json;
+    const parsed = await responseSchema.safeParseAsync(json);
+
+    if (!parsed.success) {
+      throw createSchemaError(response, parsed.error);
+    }
+
+    return parsed.data;
   },
 };
 
-function createError(response: Response) {
-  // TODO: Improve error handling and add logging
-  return new Error(`HTTP error! status: ${response.status}`);
+function createResponseError(response: Response) {
+  return new Error('<ODIN_RESPONSE_ERROR> ', {
+    cause: {
+      response: response,
+    },
+  });
+}
+
+function createJsonError(response: Response) {
+  return new Error('<ODIN_JSON_ERROR> ', { cause: response });
+}
+
+function createSchemaError(response: Response, error: ZodError) {
+  return new Error('<ODIN_SCHEMA_ERROR> ', {
+    cause: {
+      response: response,
+      schemaError: error,
+    },
+  });
+}
+
+function getUrl(endpoint: string) {
+  return env.public.odin.apiUrl + endpoint;
+}
+
+function getOptions(options?: RequestOptions) {
+  return {
+    headers: {
+      Authorization: `Token ${env.secret.odin.apiKey}`,
+      'Cache-Control': 'no-cache',
+      ...options?.headers,
+    },
+    ...options,
+  };
 }
