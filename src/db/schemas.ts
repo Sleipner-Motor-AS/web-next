1adf480ec63ea3a8e23e9fb50e502b80eb652db4
strandseter <43789401+strandseter@users.noreply.github.com>
2025-03-11 10:44:30 +0100

    feat: abstract payload and drizzle

---
+++ src/db/schemas.ts
@@ -0,0 +1,10 @@
+import { customerProductPricesTable, customersTable } from './schemas/customer';
+import { marketProductsTable, productsTable } from './schemas/product';
+
+export const allSchemas = {
+  customersTable,
+  productsTable,
+  marketProductsTable,
+  customerProductPricesTable,
+  // Always add new tables here..
+};
